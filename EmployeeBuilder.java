c3bbf2ec9431683f941247b0dc01dbb5816f1c33
DHATRIREDDY <dhatrireddy007@gmail.com>
2020-07-18 13:50:10 +0530

    added switchCase Statements

--- EmployeeBuilder.java
+++ EmployeeBuilder.java
@@ -6,21 +6,18 @@ public static void main(String[] args)
 	System.out.println("Welcome to Employee Wage Compuation Program ");
     int isPartTime=2,isFullTime=1;
     int empRatePerHr=20, full_empHrs=8,partTime_empHrs=4,salary=0;
-    double empCheck=Math.floor(Math.random()*10)%3;	   		   	
-    if( empCheck == isFullTime)
+    int empCheck=(int)Math.floor(Math.random()*10)%3;
+    switch(empCheck)
     {
-    	System.out.println("Employee is Present for FullTime");
-        salary=empRatePerHr*full_empHrs;
-    }
-    else if( empCheck == isPartTime)
-    {
-    	System.out.println("Employee is Present for PartTime");
-    	salary=empRatePerHr*partTime_empHrs;
-    }
-    else
-    {
-    	System.out.println("Employee is Abscent");
-        salary=0;
+      case 1:
+    	 System.out.println("Employee is Present for FullTime");
+         salary=empRatePerHr*full_empHrs;
+         break;
+      case 2:
+    	 System.out.println("Employee is Present for PartTime");
+    	 salary=empRatePerHr*partTime_empHrs;
+    	 break;
+      default:System.out.println("Employee is Abscent");
     }
     System.out.println("Salary:"+salary);
 }
