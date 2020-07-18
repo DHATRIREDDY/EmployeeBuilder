3301a89f05f86e2c574c918b46262f662a5d001b
DHATRIREDDY <dhatrireddy007@gmail.com>
2020-07-18 12:39:08 +0530

    added EmployeeAttendance

--- EmployeeBuilder.java
+++ EmployeeBuilder.java
@@ -4,13 +4,23 @@ public class EmployeeBuilder {
 public static void main(String[] args)
 {
 	System.out.println("Welcome to Employee Wage Compuation Program ");
-    int IS_FULL_TIME=1;
+    int isPresent=1;
+    int empRatePerHr=20, empHrs=8,salary=0;
     double empCheck=Math.floor(Math.random()*10)%2;
-    if(empCheck == IS_FULL_TIME)
+    if(empCheck == isPresent)
     	System.out.println("Employee is Present");
     else
     	System.out.println("Employee is Abscent");
-    	
+    if( empCheck == isPresent)
+    {
+    	salary=(empHrs*empRatePerHr);
+    	System.out.println(salary);
+    }
+    else
+    {
+    	salary=0;
+    	System.out.println(salary);
+    }	   	
 }
 
 }
