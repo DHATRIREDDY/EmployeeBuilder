9c003f0ffd95d623764176711bd7e463006b06f0
DHATRIREDDY <dhatrireddy007@gmail.com>
2020-07-18 13:39:10 +0530

    added PartTimeWage

--- EmployeeBuilder.java
+++ EmployeeBuilder.java
@@ -4,12 +4,25 @@ public class EmployeeBuilder {
 public static void main(String[] args)
 {
 	System.out.println("Welcome to Employee Wage Compuation Program ");
-    int isPresent=1;
-    double empCheck=Math.floor(Math.random()*10)%2;
-    if(empCheck == isPresent)
-    	System.out.println("Employee is Present");
+    int isPartTime=2,isFullTime=1;
+    int empRatePerHr=20, full_empHrs=8,partTime_empHrs=4,salary=0;
+    double empCheck=Math.floor(Math.random()*10)%3;	   		   	
+    if( empCheck == isFullTime)
+    {
+    	System.out.println("Employee is Present for FullTime");
+        salary=empRatePerHr*full_empHrs;
+    }
+    else if( empCheck == isPartTime)
+    {
+    	System.out.println("Employee is Present for PartTime");
+    	salary=empRatePerHr*partTime_empHrs;
+    }
     else
-    	System.out.println("Employee is Abscent");	   	
+    {
+    	System.out.println("Employee is Abscent");
+        salary=0;
+    }
+    System.out.println("Salary:"+salary);
 }
 
 }
