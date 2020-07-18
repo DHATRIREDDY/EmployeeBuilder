e0f14329ee9657fc4d7c2ad52ce31cfa9ad6c751
DHATRIREDDY <dhatrireddy007@gmail.com>
2020-07-18 12:50:11 +0530

    modified EmployeeWage

--- EmployeeBuilder.java
+++ EmployeeBuilder.java
@@ -4,5 +4,21 @@ public class EmployeeBuilder {
 public static void main(String[] args)
 {
 	System.out.println("Welcome to Employee Wage Compuation Program ");
+    int isPresent=1;
+    int empRatePerHr=20, empHrs=8,salary=0;
+    double empCheck=Math.floor(Math.random()*10)%2;
+    if(empCheck == isPresent)
+    {
+    	System.out.println("Employee is Present");
+        salary=(empHrs*empRatePerHr);
+	    System.out.println("Salary:"+salary);
+    }
+    else
+    {
+    	System.out.println("Employee is Abscent");
+    	salary=0;
+    	System.out.println("Salary:"+salary);
+    }	   	
 }
+
 }
