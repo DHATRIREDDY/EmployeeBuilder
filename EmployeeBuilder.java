a7d1961eff745847faf62321d60ab3cb2d4e5dcc
DHATRIREDDY <dhatrireddy007@gmail.com>
2020-07-18 14:02:28 +0530

    added wagesForMonth

--- EmployeeBuilder.java
+++ EmployeeBuilder.java
@@ -6,20 +6,23 @@ public static void main(String[] args)
 	System.out.println("Welcome to Employee Wage Compuation Program ");
     int isPartTime=2,isFullTime=1;
     int empRatePerHr=20, full_empHrs=8,partTime_empHrs=4,salary=0;
-    int empCheck=(int)Math.floor(Math.random()*10)%3;
-    switch(empCheck)
+    int daysPerMonth=20;
+    for(int i=1;i<=daysPerMonth;i++)
     {
-      case 1:
-    	 System.out.println("Employee is Present for FullTime");
-         salary=empRatePerHr*full_empHrs;
-         break;
-      case 2:
-    	 System.out.println("Employee is Present for PartTime");
-    	 salary=empRatePerHr*partTime_empHrs;
-    	 break;
-      default:System.out.println("Employee is Abscent");
+      int empCheck=(int)Math.floor(Math.random()*10)%3;
+      switch(empCheck)
+      {
+        case 1:
+          salary=salary+empRatePerHr*full_empHrs;
+          break;
+        case 2:
+    	  salary=salary+empRatePerHr*partTime_empHrs;
+    	  break;
+        default:
+        	salary=salary+0;
+      }
     }
-    System.out.println("Salary:"+salary);
+    System.out.println("Wages for Month:"+salary);
 }
 
 }
