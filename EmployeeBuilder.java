96a11890916b591045fc644239ad74d206af7755
DHATRIREDDY <dhatrireddy007@gmail.com>
2020-07-20 13:36:56 +0530

    Refactor:using methods

--- EmployeeBuilder.java
+++ EmployeeBuilder.java
@@ -1,33 +1,42 @@
 package programscc;
 
 public class EmployeeBuilder {
-public static void main(String[] args)
-{
-	System.out.println("Welcome to Employee Wage Compuation Program ");
-    int isPartTime=2,isFullTime=1;
-    int empRatePerHr=20, full_empHrs=8,partTime_empHrs=4,salary=0;
-    int daysPerMonth=20,workingHrs=0,workingDays=0;
-    while(workingHrs<100 && workingDays<20)
-    {
-      int empCheck=(int)Math.floor(Math.random()*10)%3;
-      switch(empCheck)
-      {
-        case 1:
-          salary=salary+empRatePerHr*full_empHrs;
-          workingHrs=workingHrs+full_empHrs;
-          break;
-        case 2:
-    	  salary=salary+empRatePerHr*partTime_empHrs;
-    	  workingHrs=workingHrs+partTime_empHrs;
-    	  break;
-        default:
-        	salary=salary+0;
-      }
-      workingDays++;
-    }
-    System.out.println("Working Hours:"+workingHrs);
-    System.out.println("Working Days:"+ workingDays);
-    System.out.println("Wages for Month:"+salary);
-}
+	static final int isPartTime=1;
+    static final int isFullTime=2;
+    static final int empRatePerHr=20;
+    static final int maxHrsMonth=100;
+    static final int workingDays=20;
+    void empWageComputation()
+	{
+	    int empHrs=0,salary=0,workingHrs=0,totalworkingDays=0,totalEmpHrs=0,totalSalary=0;
+	    System.out.println("Day\tEmpHrs\tSalary\tTotalEmpHrs");
+	    while(workingHrs<=maxHrsMonth && totalworkingDays<workingDays)
+	    {
+	      totalworkingDays++;
+	      int empCheck=(int)Math.floor(Math.random()*10)%3;
+	      switch(empCheck)
+	      {
+	         case  isPartTime:
+	        	 empHrs=4; 
+	             break;
+	         case isFullTime:
+	        	 empHrs=8;
+	    	      break;
+	         default:
+	        	empHrs=0;
+	     }
+	      salary=empHrs*empRatePerHr;
+	      totalEmpHrs+=empHrs; 
+	      System.out.println(totalworkingDays+"\t"+empHrs+"\t"+salary+"\t"+totalEmpHrs);
+	    }
+	    totalSalary=totalEmpHrs*empRatePerHr;
+	    System.out.println("Total Salary:"+totalSalary);
+	}
+   public static void main(String[] args)
+   {
+	 System.out.println("Welcome to Employee Wage Compuation Program ");
+     EmployeeBuilder b=new EmployeeBuilder();
+     b.empWageComputation();
+   }
 
 }
